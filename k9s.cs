76f036c8fcdfc0d5621a7307c81bbd47aad5e0a3
faruk-guler <95317318+faruk-guler@users.noreply.github.com>
2025-02-06 23:20:17 +0300

    Update k9s.cs

--- k9s.cs
+++ k9s.cs
@@ -1,4 +1,23 @@
 #K9S SHELL
+
+# List current version
+k9s version
+
+# To get info about K9s runtime (logs, configs, etc..)
+k9s info
+
+# List all available CLI options
+k9s help
+
+# To run K9s in a given namespace
+k9s -n mycoolns
+
+# Start K9s in an existing KubeConfig context
+k9s --context coolCtx
+
+# Start K9s in readonly mode - with all cluster modification commands disabled
+k9s --readonly
+
 csr: certificatesigningrequests
 cs: componentstatuses
 cm: configmaps
@@ -21,21 +40,3 @@ rc: replicationcontrollers
 quota: resourcequotas
 sa: serviceaccounts
 svc: services
-
-# List current version
-k9s version
-
-# To get info about K9s runtime (logs, configs, etc..)
-k9s info
-
-# List all available CLI options
-k9s help
-
-# To run K9s in a given namespace
-k9s -n mycoolns
-
-# Start K9s in an existing KubeConfig context
-k9s --context coolCtx
-
-# Start K9s in readonly mode - with all cluster modification commands disabled
-k9s --readonly
